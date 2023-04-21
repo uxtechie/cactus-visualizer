e563d340489b7a80a67c066c698ba80af3650e42
Julián Mulet <>
2023-04-21 20:19:08 +0200

    refactor: Remove MainLayout from _app Next resource

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react'
 import type { AppProps } from 'next/app'
 import '@Styles/globals.css'
-import { MainLayout } from 'src/shared/layouts/MainLayout'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <Component {...pageProps} />
   )
 }
 
