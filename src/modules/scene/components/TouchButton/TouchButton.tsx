0459b945ea33e29ff3c5cca1692f03b10a427288
Julián Mulet <>
2023-04-23 20:14:11 +0200

    refactor: fix TouchButton responsiveness and semantic

--- src/modules/scene/components/TouchButton/TouchButton.tsx
+++ src/modules/scene/components/TouchButton/TouchButton.tsx
@@ -2,30 +2,30 @@ import { GenericItemHandler } from '@Types'
 import { PointModel } from '@Models/point'
 import { IconFingerprint } from '@Icons/IconFingerPrint'
 
-export interface TouchButtonProps {
+interface TouchButtonProps {
   onClickHandler: GenericItemHandler<PointModel>
   point: PointModel
 }
 
 const TouchButton: React.FC<TouchButtonProps> = ({ point, onClickHandler }) => {
   return (
-    <div
+    <button
       style={{
         top: `${point.coordY}%`,
         left: `${point.coordX}%`
       }}
-      className='absolute rounded-full w-11 h-11 transition hover:opacity-30'
+      className='absolute -translate-y-5 -translate-x-5 rounded-full w-10 h-10 transition hover:opacity-30'
       onClick={() => onClickHandler(point)}
     >
-      <div className='relative w-full h-full flex items-center justify-center'>
-        <div
+      <span className='relative w-full h-full flex items-center justify-center'>
+        <span
           className='absolute w-full h-full rounded-full outline bg-neutral-800 outline-neutral-800 opacity-40'
         />
         <IconFingerprint
           className='w-full h-full text-white p-1.5 rotate-12 border-2 rounded-full'
         />
-      </div>
-    </div>
+      </span>
+    </button>
   )
 }
 
