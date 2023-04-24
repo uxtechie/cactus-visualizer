ef13c9413ba05d67701325b81ef8886b8accacd8
Julián Mulet <>
2023-04-24 15:28:22 +0200

    build: enable jest & editor config & pnpm audit postinstall
    set new scripts to package.json

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1'
+  }
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
