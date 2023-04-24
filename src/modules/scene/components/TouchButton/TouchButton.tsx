79170bd7db0c9f06080bdbfaa2defbdeb3d6e2da
Julián Mulet <>
2023-04-24 09:35:10 +0200

    feat: add loading animaion to TouchButton

--- src/modules/scene/components/TouchButton/TouchButton.tsx
+++ src/modules/scene/components/TouchButton/TouchButton.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react'
 import { GenericItemHandler } from '@Types'
 import { PointModel } from '@Models/point'
 import { IconFingerprint } from '@Icons/IconFingerPrint'
@@ -5,16 +6,21 @@ import { IconFingerprint } from '@Icons/IconFingerPrint'
 interface TouchButtonProps {
   onClickHandler: GenericItemHandler<PointModel>
   point: PointModel
+  loading?: boolean
 }
 
-const TouchButton: React.FC<TouchButtonProps> = ({ point, onClickHandler }) => {
+const TouchButton: FC<TouchButtonProps> = ({ point, onClickHandler, loading = false }) => {
   return (
     <button
       style={{
         top: `${point.coordY}%`,
         left: `${point.coordX}%`
       }}
-      className='absolute -translate-y-5 -translate-x-5 rounded-full w-10 h-10 transition hover:opacity-30'
+      className={`
+      absolute -translate-y-4 -translate-x-4 rounded-full w-8 h-8
+      transition hover:opacity-30
+      lg:-translate-y-5 lg:-translate-x-5 lg:w-10 lg:h-10
+      `}
       onClick={() => onClickHandler(point)}
     >
       <span className='relative w-full h-full flex items-center justify-center'>
@@ -22,7 +28,11 @@ const TouchButton: React.FC<TouchButtonProps> = ({ point, onClickHandler }) => {
           className='absolute w-full h-full rounded-full outline bg-neutral-800 outline-neutral-800 opacity-40'
         />
         <IconFingerprint
-          className='w-full h-full text-white p-1.5 rotate-12 border-2 rounded-full'
+          className={`
+          ${loading ? 'animate-pulse' : ''}
+          w-full h-full text-white p-1 rotate-12 border-2 rounded-full
+          lg:p-1.5
+          `}
         />
       </span>
     </button>
