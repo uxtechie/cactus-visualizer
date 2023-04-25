d11aaadfdca405cb6d53512ee122689fc963a87e
Julián Mulet <>
2023-04-25 15:05:53 +0200

    build: remove uneeded path alias config

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,23 +8,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@Pages/(.*)$': '<rootDir>/src/pages/$1',
-    '^@Components/(.*)$': '<rootDir>/src/shared/components/$1',
-    '^@Layouts/(.*)$': '<rootDir>/src/shared/layouts/$1',
-    '^@Modules/(.*)$': '<rootDir>/src/modules/$1',
-    '^@Hooks/(.*)$': '<rootDir>/src/shared/hooks/$1',
-    '^@Models/(.*)$': '<rootDir>/src/shared/models/$1',
-    '^@Utils/(.*)$': '<rootDir>/src/lib/utils/$1',
-    '^@Styles/(.*)$': '<rootDir>/src/styles/$1',
-    '^@Services/(.*)$': '<rootDir>/src/services/$1',
-    '^@Constants/(.*)$': '<rootDir>/src/shared/constants/$1',
-    '^@Images/(.*)$': '<rootDir>/src/assets/images/$1',
-    '^@Icons/(.*)$': '<rootDir>/src/assets/icons/$1',
-    '^@Types': '<rootDir>/src/shared/types.ts',
-    '^@/(.*)$': '<rootDir>/$1'
-  }
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,30 +5,6 @@ const nextConfig = {
     domains: ['firebasestorage.googleapis.com'],
     // tailwind breakpoints, min: sm, md, lg, xl, 2xl
     deviceSizes: [640, 768, 1024, 1280, 1536]
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.resolve.alias = {
-      ...config.resolve.alias
-    }
-    config.resolve.alias['@Pages'] = path.join(__dirname, './src/pages')
-    config.resolve.alias['@Components'] = path.join(__dirname, './src/shared/components')
-    config.resolve.alias['@Layouts'] = path.join(__dirname, './src/shared/layouts')
-    config.resolve.alias['@Modules'] = path.join(__dirname, './src/modules')
-    config.resolve.alias['@Hooks'] = path.join(__dirname, './src/shared/hooks')
-    config.resolve.alias['@Models'] = path.join(__dirname, './src/shared/models')
-    config.resolve.alias['@Utils'] = path.join(__dirname, './src/lib/utils')
-    config.resolve.alias['@Styles'] = path.join(__dirname, './src/styles')
-    config.resolve.alias['@Services'] = path.join(__dirname, './src/services')
-    config.resolve.alias['@Constants'] = path.join(__dirname, './src/shared/constants')
-    config.resolve.alias['@Images'] = path.join(__dirname, './src/assets/images')
-    config.resolve.alias['@Icons'] = path.join(__dirname, './src/assets/icons')
-    config.resolve.alias['@Types'] = path.join(__dirname, './src/shared/types.ts')
-    config.resolve.alias['@'] = path.join(__dirname, './')
-
-    return config
   }
 }
 
